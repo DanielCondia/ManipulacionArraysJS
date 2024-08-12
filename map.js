5a6badf3d94924e4f8cb642955f43a474cca5ab6
Daniel Condía Figueredo <dcondia@clarisacloud.com>
2024-08-12 12:15:05 -0500

    :sparkles: Se agrega documentacion y ejemplo de map en js

---
+++ map.js
@@ -0,0 +1,24 @@
+function imprimir(arrayOriginal, arrayNuevo) {
+	console.log('Array Origina => ', arrayOriginal);
+	console.log('Array Editado => ', arrayNuevo);
+}
+
+console.log('Ejemplo de manipulación del array sin el map');
+
+const letras = ['a', 'b', 'c', 'd'];
+
+// Ejemplo de manipulación del array sin el map
+const nuevoArray = [];
+for (let i = 0; i < letras.length; i++) {
+	const elemento = letras[i];
+	nuevoArray.push(elemento + '++');	// Editamos el elemento y se agrega un ++
+}
+
+imprimir(letras, nuevoArray);
+console.log('\n');
+
+console.log('Ejemplo de manipulación del array con el map');
+// Ejemplo de manipulación del array con el map
+const nuevoArray2 = letras.map(item => item + '++');
+imprimir(letras, nuevoArray2);
+
