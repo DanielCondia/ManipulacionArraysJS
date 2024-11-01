ed8842de188a0bae822a7240b129a68e9e5ca662
Daniel Condía Figueredo <dcondia@clarisacloud.com>
2024-11-01 15:22:47 -0500

    :sparkles: Se agrega ejemplo y documentacion de flat js

---
+++ flat/flat.js
@@ -0,0 +1,26 @@
+// Ejemplo largo
+let arreglo = [
+	[1,2,3],
+	[4,5,6,[4,5,6,[7,8,9]]],
+	[7,8,9,[10,11,12,[13,14,15,[13,14,15]]]]
+];
+
+let arregloPlano = [];
+
+function aplanarArreglo(subArreglo) {
+	for (let i = 0; i < subArreglo.length; i++) {
+		let valor = subArreglo[i];
+		if (Array.isArray(valor)) {
+			aplanarArreglo(valor);
+			continue;
+		}
+		arregloPlano.push(valor);
+	}
+}
+
+aplanarArreglo(arreglo);
+
+console.log('arregloPlano => ', arregloPlano);
+
+const rtaFlat = arreglo.flat(3); // Se específica hasta que nivel se tiene que aplanar.
+console.log('rtaFlat => ', rtaFlat);
